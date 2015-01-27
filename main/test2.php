698b917983ce278bfe8ac5252629d54de347430e
duckmin <allthingsrobert@gmail.com>
2015-01-26 19:58:47 -0500

    x

--- main/test2.php
+++ main/test2.php
@@ -1,19 +1,19 @@
 <?php
 $server = dirname(__FILE__)."/../server";
 	
-	include_once $server."/configs.php";
-	try{
+include_once $server."/configs.php";
+	//try{
 	//$db = new MongoClient();
-	}catch( MongoCursorException $e ) {	
+	//}catch( MongoCursorException $e ) {	
 		//	echo $e->getMessage()."\n";
-	}
+	//}
 	
-	phpinfo();
+	//phpinfo();
 	//$dt = date('Y-m-d'); //todays date with no time
 	
 	//$ts = new MongoDate( strtotime( $dt." 00:00:00" ) ); //time of 00:00:00 because we only want 1 record per page per day
-	$url = "http://localhost:8080/video";	
-		echo "sdsad";
+	//$url = "http://localhost:8080/video";	
+		//echo "sdsad";
 	//$write_result = $db->blog->analytics->update( array("url"=>$url,'date'=>$ts), array('$inc'=>array('views'=>1)), array('upsert'=>true) );
 	//$written = ( $write_result['n'] === 1 )? true : false;				
 	
@@ -23,5 +23,20 @@ $server = dirname(__FILE__)."/../server";
 	foreach( $cursor as $post ){
 		echo $post["views"]."<br>";
 	}*/
-	echo  var_dump( filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP) );
+	////echo  var_dump( filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP) );
+	
+	
+	$src = $GLOBALS['index_path']."/pics/amazon/dove.jpg";
+	$img_info = getimagesize($src);
+	echo var_dump($img_info);
+	$width = $img_info[0];
+	$height = $img_info[1];
+	$aspect_width = 50;
+	$aspect_height = round( $height / $width * $aspect_width );
+	//imagescale ( $img , $aspect_width, $aspect_height );
+	$img_p = imagecreatetruecolor( $aspect_width, $aspect_height ); //frame for img to be copied into
+	$img = imagecreatefromjpeg( $src );
+	imagecopyresampled($img_p, $img, 0, 0, 0, 0, $aspect_width, $aspect_height, $width, $height);
+	imagejpeg($img_p, $GLOBALS['index_path']."/pics/amazon/dove-thumb.jpg", 100); //save resized img canvas to new src
+
 ?>

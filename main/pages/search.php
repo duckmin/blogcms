6fa2f48447e36d38764a750afbb7160c37c94abc
duckmin <allthingsrobert@gmail.com>
2015-03-30 12:37:37 -0400

    removed old file refs

--- main/pages/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-	echo "search";
-	//get the vars and rediect to pretty url
-	$cat = $_GET["cat"];
-	$s = $_GET["search"];
-	$search = ( strlen( $s ) > 0 )? "/".urlencode( $s ) : "";
-	$redirect_url = $GLOBALS['base_url']."/".$cat.$search."/1";
-	header("location:".$redirect_url );
-?>

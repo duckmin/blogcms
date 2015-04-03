2360a0179b6a814c36d599a2ab460c80d5b1c782
root <root@babootron.com>
2015-04-02 21:01:25 -0400

    changed manager to staic funcs

--- server/classes/ManagerActions.php
+++ server/classes/ManagerActions.php
@@ -5,11 +5,8 @@
        
         public $template_name;
        
-        public function __construct(){
-            
-        }
 		
-		public function isLoggedIn( ){
+		public static function isLoggedIn( ){
             if( isset( $_SESSION['user'] ) && isset( $_SESSION['level'] ) ){
             	return true;
             }else{
@@ -17,12 +14,12 @@
             }
         }
         
-        public function authenticate(){
+        public static function authenticate(){
             header('WWW-Authenticate: Basic realm="Manager"');
 			header('HTTP/1.0 401 Unauthorized');
         }
         
-         public function loginSuccess( $user, $pw ){
+         public static function loginSuccess( $user, $pw ){
 			$users = json_decode( file_get_contents( $GLOBALS['server_path']."/includes/logins.json" ), true );  
 			          
          if( array_key_exists( $user, $users ) && $users[$user]["pw"] === $pw ){	

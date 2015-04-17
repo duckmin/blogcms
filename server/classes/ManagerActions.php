1cd91afbfef65c609e486fd8d6e2610491d63c39
duckmin <allthingsrobert@gmail.com>
2015-04-17 16:50:50 -0400

    removed old code restyled page header a bit

--- server/classes/ManagerActions.php
+++ server/classes/ManagerActions.php
@@ -2,36 +2,33 @@
 	require_once dirname(__FILE__)."/../configs.php";
 		
 	class ManagerActions {
-       
-        public $template_name;
-       
-		
+ 
 		public static function isLoggedIn( ){
-            if( isset( $_SESSION['user'] ) && isset( $_SESSION['level'] ) ){
-            	return true;
-            }else{
-            	return false;	
-            }
-        }
-        
-        public static function authenticate(){
-            header('WWW-Authenticate: Basic realm="Manager"');
+			if( isset( $_SESSION['user'] ) && isset( $_SESSION['level'] ) ){
+				return true;
+			}else{
+				return false;	
+			}
+		}
+		  
+		public static function authenticate(){
+			header('WWW-Authenticate: Basic realm="Manager"');
 			header('HTTP/1.0 401 Unauthorized');
-        }
-        
-         public static function loginSuccess( $user, $pw ){
+		}
+		  
+		public static function loginSuccess( $user, $pw ){
 			$users = json_decode( file_get_contents( $GLOBALS['server_path']."/includes/logins.json" ), true );  
-			          
-         if( array_key_exists( $user, $users ) && $users[$user]["pw"] === $pw ){	
-            $_SESSION['user'] = $user;
+		 
+			if( array_key_exists( $user, $users ) && $users[$user]["pw"] === $pw ){	
+				$_SESSION['user'] = $user;
 				$_SESSION['level'] = $users[$user]["level"];
 				$_SESSION['created'] = time();
 				return true;
 			}else{
 				return false;	
 			}
-        }
-   
-    }
+		}
+	
+	}
    
 ?>
